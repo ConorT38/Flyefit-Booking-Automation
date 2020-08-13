8eb63eb7d7d419f3dcd094bfe8d87c144c46f8ad
unknown <Conor@cothom-devbox.europe.corp.microsoft.com>
2020-08-13 13:21:36 +0100

    added user preferences for timeslot

--- flyefit_booking.py
+++ flyefit_booking.py
@@ -1,15 +1,34 @@
 import time
+import enum
 import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options  
 
+class TimeSlot:
+    _5_30 = 0
+    _7_00 = 1
+    _8_30 = 2
+    _10_00 = 3
+    _11_30 = 4
+    _13_00 = 5
+    _14_30 = 6
+    _16_00 = 7
+    _17_30 = 8
+    _19_00 = 9
+    _20_30 = 10
+    _22_00 = 11
+
 chrome_options = Options()  
 chrome_options.add_argument("--headless")  
 
-
+# Enter user preferences here:
+########################
 EMAIL = ""
 PASSWORD = ""
+TIMESLOT = TimeSlot._7_00
+########################
+
 today = str(datetime.date.today() + datetime.timedelta(days=1))
 
 driver = webdriver.Chrome('chromedriver', chrome_options=chrome_options)  # Optional argument, if not specified will search path.
@@ -26,10 +45,10 @@ login_button.click()
 
 driver.get('https://myflye.flyefit.ie/myflye/book-workout/167/12/'+today)
 booking_slot = driver.find_elements_by_class_name("btn-primary")
-booking_slot[1].click()
+booking_slot[TIMESLOT].click()
 
 book_button = driver.find_element_by_name('book_class')
 driver.execute_script("arguments[0].click();", book_button)
 
-time.sleep(5)
+time.sleep(3)
 driver.quit()
